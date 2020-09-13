4c1480e9229958b5dff7682893a8beca2ab4ecf2
Vitaliy Maksimov <vetal199624@mail.ru>
2020-09-13 17:19:54 +0300

    Updates

---
+++ precache-manifest.f97e3e40136392c9c93c44e25bcddbfe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6745d03706061211dec0bf5462450b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ff83fea113939f7760d",
+    "url": "./static/css/main.21936807.chunk.css"
+  },
+  {
+    "revision": "6c963f7b869836e9343f",
+    "url": "./static/js/2.19370af8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.19370af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff83fea113939f7760d",
+    "url": "./static/js/main.3590ce01.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "./static/media/add-button.b2fce4ac.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "./static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "./static/media/edit-avatar-pencil.d476a8b8.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "./static/media/edit-button.b05083e5.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "./static/media/error.4072126c.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "./static/media/like-button-active.9fdebb6b.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "./static/media/like-button.a8acc871.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "./static/media/logo.c6f11019.svg"
+  },
+  {
+    "revision": "5df9ea80259cbb131b25d5648d497d91",
+    "url": "./static/media/mobile-menu.5df9ea80.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "./static/media/remove-card-button.efa6fb04.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "./static/media/success.e38b395c.svg"
+  }
+]);
